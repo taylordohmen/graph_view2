676a5d220293c01839c5a176f3f390ad7de03855
Taylor Dohmen <taylorthedohmen@gmail.com>
2024-11-01 01:24:59 -0600

    computes Louvain communities and uses these to determine layout

--- main.ts
+++ main.ts
@@ -1,166 +1,214 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, WorkspaceLeaf } from 'obsidian';
 import { SigmaGraphView, VIEW_TYPE_SIGMA } from 'sigma-graph-view';
 
-interface SigmaGraphPluginSettings {
-	mySetting: string;
-}
+// interface SigmaGraphPluginSettings {
+// 	mySetting: string;
+// }
 
-const DEFAULT_SETTINGS: SigmaGraphPluginSettings = {
-	mySetting: 'default'
-}
+// const DEFAULT_SETTINGS: SigmaGraphPluginSettings = {
+// 	mySetting: 'default'
+// }
 
+// export default class SigmaGraphPlugin extends Plugin {
+
+//   settings: SigmaGraphPluginSettings;
+
+// 	async onload() {
+// 		await this.loadSettings();
+
+//     console.log("Loading Sigma Graph View...");
+
+//     this.registerView(
+//       VIEW_TYPE_SIGMA,
+//       (leaf) => new SigmaGraphView(leaf)
+//     );
+
+//     // Add command to open custom graph view
+//     this.addRibbonIcon("dot-network", "Open Custom Graph View", () => {
+//       this.openSigmaGraphView();
+//     });
+
+// 		// This creates an icon in the left ribbon.
+// 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+// 			// Called when the user clicks the icon.
+// 			new Notice('This is a notice!');
+// 		});
+// 		// Perform additional things with the ribbon
+// 		ribbonIconEl.addClass('my-plugin-ribbon-class');
+
+// 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+// 		const statusBarItemEl = this.addStatusBarItem();
+// 		statusBarItemEl.setText('Status Bar Text');
+
+// 		// This adds a simple command that can be triggered anywhere
+// 		this.addCommand({
+// 			id: 'open-sample-modal-simple',
+// 			name: 'Open sample modal (simple)',
+// 			callback: () => {
+// 				new SampleModal(this.app).open();
+// 			}
+// 		});
+// 		// This adds an editor command that can perform some operation on the current editor instance
+// 		this.addCommand({
+// 			id: 'sample-editor-command',
+// 			name: 'Sample editor command',
+// 			editorCallback: (editor: Editor, view: MarkdownView) => {
+// 				console.log(editor.getSelection());
+// 				editor.replaceSelection('Sample Editor Command');
+// 			}
+// 		});
+// 		// This adds a complex command that can check whether the current state of the app allows execution of the command
+// 		this.addCommand({
+// 			id: 'open-sample-modal-complex',
+// 			name: 'Open sample modal (complex)',
+// 			checkCallback: (checking: boolean) => {
+// 				// Conditions to check
+// 				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+// 				if (markdownView) {
+// 					// If checking is true, we're simply "checking" if the command can be run.
+// 					// If checking is false, then we want to actually perform the operation.
+// 					if (!checking) {
+// 						new SampleModal(this.app).open();
+// 					}
+
+// 					// This command will only show up in Command Palette when the check function returns true
+// 					return true;
+// 				}
+// 			}
+// 		});
+
+// 		// This adds a settings tab so the user can configure various aspects of the plugin
+// 		this.addSettingTab(new SigmaGraphSettingTab(this.app, this));
+
+// 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+// 		// Using this function will automatically remove the event listener when this plugin is disabled.
+// 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+// 			console.log('click', evt);
+// 		});
+
+// 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+// 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+// 	}
+
+// 	onunload() {
+
+// 	}
+
+// 	async loadSettings() {
+// 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+// 	}
+
+// 	async saveSettings() {
+// 		await this.saveData(this.settings);
+// 	}
+
+//     async openSigmaGraphView() {
+//         const { workspace } = this.app;
+
+//         let leaf: WorkspaceLeaf | null = null;
+//         const leaves = workspace.getLeavesOfType(VIEW_TYPE_SIGMA);
+
+//         if (leaves.length > 0) {
+//             // A leaf with our view already exists, use that
+//             leaf = leaves[0];
+//         } else {
+//             // Our view could not be found in the workspace, create a new leaf
+//             leaf = workspace.getLeaf();
+//             await leaf.setViewState({ type: VIEW_TYPE_SIGMA, active: true });
+//         }
+//         workspace.revealLeaf(leaf);
+//     }
+// }
+
+// class SampleModal extends Modal {
+// 	constructor(app: App) {
+// 		super(app);
+// 	}
+
+// 	onOpen(): void {
+// 		const {contentEl} = this;
+// 		contentEl.setText('Woah!');
+// 	}
+
+// 	onClose(): void {
+// 		const {contentEl} = this;
+// 		contentEl.empty();
+// 	}
+// }
+
+// class SigmaGraphSettingTab extends PluginSettingTab {
+// 	plugin: SigmaGraphPlugin;
+
+// 	constructor(app: App, plugin: SigmaGraphPlugin) {
+// 		super(app, plugin);
+// 		this.plugin = plugin;
+// 	}
+
+// 	display(): void {
+// 		const {containerEl} = this;
+
+// 		containerEl.empty();
+
+// 		new Setting(containerEl)
+// 			.setName('Setting #1')
+// 			.setDesc('It\'s a secret')
+// 			.addText(text => text
+// 				.setPlaceholder('Enter your secret')
+// 				.setValue(this.plugin.settings.mySetting)
+// 				.onChange(async (value) => {
+// 					this.plugin.settings.mySetting = value;
+// 					await this.plugin.saveSettings();
+// 				})
+//             );
+// 	}
+// } */
+
+// Main plugin class
 export default class SigmaGraphPlugin extends Plugin {
-	
-  settings: SigmaGraphPluginSettings;
+    async onload() {
+        // Register the custom view
+        this.registerView(
+            VIEW_TYPE_SIGMA, 
+            (leaf: WorkspaceLeaf) => new SigmaGraphView(leaf)
+        );
 
-	async onload() {
-		await this.loadSettings();
+        // Add ribbon icon
+        this.addRibbonIcon(
+            'dot-network',
+            'Open Sigma Graph View',
+            (evt: MouseEvent) => {
+                this.activateView();
+            }
+        );
 
-    console.log("Loading Sigma Graph View...");
-
-    this.registerView(
-      VIEW_TYPE_SIGMA,
-      (leaf) => new SigmaGraphView(leaf)
-    );
-
-    // Add command to open custom graph view
-    this.addRibbonIcon("dot-network", "Open Custom Graph View", () => {
-      this.openSigmaGraphView();
-    });
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SigmaGraphSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-
-  async openSigmaGraphView() {
-    const { workspace } = this.app;
-
-    let leaf: WorkspaceLeaf | null = null;
-    const leaves = workspace.getLeavesOfType(VIEW_TYPE_SIGMA);
-
-    if (leaves.length > 0) {
-      // A leaf with our view already exists, use that
-      leaf = leaves[0];
-    } else {
-      // Our view could not be found in the workspace, create a new leaf
-      leaf = workspace.getLeaf();
-      await leaf.setViewState({ type: VIEW_TYPE_SIGMA, active: true });
+        // Add command to open graph view
+        this.addCommand({
+            id: 'open-sigma-graph-view',
+            name: 'Open Sigma Graph View',
+            callback: () => {
+                this.activateView();
+            }
+        });
     }
 
-    // "Reveal" the leaf in case it is in a collapsed sidebar
-    workspace.revealLeaf(leaf);
-  }
+    async onunload() { }
 
-}
+    async activateView() {
+        const { workspace } = this.app;
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+        let leaf: WorkspaceLeaf | null;
+        const leaves = workspace.getLeavesOfType(VIEW_TYPE_SIGMA);
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+        if (leaves.length > 0) {
+            // View is already open
+            leaf = leaves[0];
+        } else {
+            // Create new leaf
+            leaf = workspace.getLeaf();
+            await leaf.setViewState({type: VIEW_TYPE_SIGMA, active: true});
+        }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SigmaGraphSettingTab extends PluginSettingTab {
-	plugin: SigmaGraphPlugin;
-
-	constructor(app: App, plugin: SigmaGraphPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        // Reveal the leaf
+        workspace.revealLeaf(leaf);
+    }
 }
--- sigma-graph-view.ts
+++ sigma-graph-view.ts
@@ -1,46 +1,176 @@
 import { ItemView, WorkspaceLeaf } from 'obsidian';
-import Graph from "graphology";
-import Sigma from "sigma";
+import Graph from 'graphology';
+import { random, circular, circlepack } from 'graphology-layout';
+import forceAtlas2 from 'graphology-layout-forceatlas2';
+import FA2Layout from 'graphology-layout-forceatlas2/worker';
+import NoverlapLayout from 'graphology-layout-noverlap/worker';
+import louvain from 'graphology-communities-louvain';
+import Sigma from 'sigma';
 
 export const VIEW_TYPE_SIGMA = 'sigma-graph-view';
 
+// Custom View class for the graph
 export class SigmaGraphView extends ItemView {
-  graph: Graph;
-  sigmaInstance: Sigma | null;
+    private container: HTMLElement;
+    private graph: Graph;
+    private renderer: Sigma;
 
-  constructor(leaf: WorkspaceLeaf) {
-    super(leaf);
+    constructor(leaf: WorkspaceLeaf) {
+        super(leaf);
+        this.container = this.contentEl.createDiv({ cls: 'sigma-graph-container' });
+    }
 
-    // Initialize graphology graph
-    this.graph = new Graph();
+    getViewType(): string {
+        return VIEW_TYPE_SIGMA;
+    }
 
-    // Example nodes and edges for demo purposes
-    this.graph.addNode("1", { label: "Node 1", x: 0, y: 0, size: 10, color: "blue" });
-    this.graph.addNode("2", { label: "Node 2", x: 1, y: 1, size: 20, color: "red" });
-    this.graph.addEdge("1", "2", { size: 5, color: "purple" });
-  }
+    getDisplayText(): string {
+        return 'Sigma Graph View';
+    }
 
-  getViewType() {
-    return VIEW_TYPE_SIGMA;
-  }
+    async onOpen() {
+        // Initialize the graph
+        this.initializeGraph();
+        // Build the graph data
+        await this.buildGraphData();
+        // Render the graph
+        await this.renderGraph();
+    }
 
-  getDisplayText() {
-    return 'Sigma Graph View';
-  }
+    async onClose() {
+        if (this.renderer) {
+            this.renderer.kill();
+        }
+    }
 
-  async onOpen() {
-    const container = this.containerEl.children[1];
-    container.empty();
-    const div = container.createEl('div');
-    div.style.width = "100%";
-    div.style.height = "100%";
-    div.style.position = "absolute";
-    div.style.top = "0";
-    div.style.left = "0";
-    this.sigmaInstance = new Sigma(this.graph, div);
-  }
+    private initializeGraph() {
+        this.graph = new Graph({ type: 'undirected' });
+    }
 
-  async onClose() {
-    // Nothing to clean up.
-  }
+    private async buildGraphData() {
+        const files = this.app.vault.getMarkdownFiles();
+
+        // Add nodes for each file
+        for (const file of files) {
+            this.graph.addNode(file.path, {
+                label: file.basename,
+                size: 1,
+                color: file.parent?.name === 'People' ? '#67B7D1' : '#bababa'
+            });
+        }
+
+        // Add edges based on links between files
+        for (const file of files) {
+            const links = this.app.metadataCache.getFileCache(file)?.links || [];
+            for (const link of links) {
+                const targetFile = this.app.metadataCache.getFirstLinkpathDest(link.link, file.path);
+                if (targetFile) {
+                    try {
+                        this.graph.addEdge(file.path, targetFile.path, {
+                            weight: 1,
+                            color: '#bababa'
+                        });
+
+                        if (this.graph.getNodeAttribute(file.path, 'person')) {
+                            this.graph.updateNodeAttribute(file.path, 'size', n => n + 1/n);
+                        }
+                        else if (file.path.contains('People/')) {
+                            this.graph.setNodeAttribute(file.path, 'person', true);
+                            this.graph.updateNodeAttribute(file.path, 'size', n => n + 1/n);
+                        } else {
+                            this.graph.setNodeAttribute(file.path, 'person', false);
+                        }
+
+                        if (this.graph.getNodeAttribute(targetFile.path, 'person')) {
+                            this.graph.updateNodeAttribute(targetFile.path, 'size', n => n + 1/n);
+                        } else if (targetFile.path.contains('People/')) {
+                            this.graph.setNodeAttribute(targetFile.path, 'person', true);
+                            this.graph.updateNodeAttribute(targetFile.path, 'size', n => n + 1/n);
+                        } else {
+                            this.graph.setNodeAttribute(targetFile.path, 'person', false);
+                        }
+
+                    } catch (e) {
+                        // Handle cases where the edge already exists
+                        console.debug('Edge already exists', e);
+                    }
+                }
+            }
+        }
+    }
+
+    private async renderGraph() {
+        // Clear the container
+        this.container.empty();
+
+        // specify container dimensions and other properties
+        this.container.style.width = '100%';
+        this.container.style.height = '100%';
+        this.container.style.position = 'absolute';
+        this.container.style.top = '0';
+        this.container.style.left = '0';
+
+        louvain.assign(this.graph);
+
+        console.log(louvain.detailed(this.graph))
+
+        // assign random positions to the nodes
+        circlepack.assign(this.graph, { hierarchyAttributes: ['community'], scale: 0.25 })
+        // const fa2settings = forceAtlas2.inferSettings(this.graph);
+        // const layout = new FA2Layout(this.graph, { settings: fa2settings });
+        // const layout = new NoverlapLayout(this.graph);
+
+        // console.log('STARTING LAYOUT');
+        // layout.start()
+
+        // Configure and initialize Sigma renderer
+        this.renderer = new Sigma(this.graph, this.container, {
+            renderEdgeLabels: false,
+            defaultNodeColor: '#67B7D1',
+            defaultEdgeColor: '#bababa',
+            // defaultNodeSize: 10,
+            minCameraRatio: 0.01,
+            maxCameraRatio: 100,
+        });
+
+        // Add interactivity
+        // this.renderer.on('clickNode', ({ node }) => {
+        //     const file = this.app.vault.getAbstractFileByPath(node);
+        //     if (file instanceof TFile) {
+        //         this.app.workspace.activeLeaf.openFile(file);
+        //     }
+        // });
+
+        // Add hover effects
+        this.renderer.on('enterNode', ({ node }) => {
+            this.graph.setNodeAttribute(node, 'highlighted', true);
+            if (this.graph.getNodeAttribute(node, 'person')) {
+                this.graph.forEachNeighbor(
+                    node,
+                    (neighbor) => {
+                        if (this.graph.getNodeAttribute(neighbor, 'person')) {
+                            this.graph.setNodeAttribute(neighbor, 'highlighted', true);
+                            this.graph.setEdgeAttribute(node, neighbor, 'highlighted', true);
+                        }
+                    }
+                );
+            }
+            
+        });
+
+        this.renderer.on('leaveNode', ({ node }) => {
+            this.graph.setNodeAttribute(node, 'highlighted', false);
+            if (this.graph.getNodeAttribute(node, 'person')) {
+                this.graph.forEachNeighbor(
+                    node,
+                    (neighbor) => {
+                        if (this.graph.getNodeAttribute(neighbor, 'person')) {
+                            this.graph.setNodeAttribute(neighbor, 'highlighted', false);
+                            this.graph.setEdgeAttribute(node, neighbor, 'highlighted', false);
+                        }
+                    }
+                );
+            }
+        });
+    }
 }
